aded8ea244cbe627fd02e5d3cebf1d3a771437e4
margi-hartanto <61889250+margi-hartanto@users.noreply.github.com>
2021-10-07 12:16:31 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 temperature = 10
 
 def print_temperature():
-    #print(temperature)
-    return temperature
+    print(temperature)
+    #return temperature
 
 def test_temperature():
     assert print_temperature() == 10
